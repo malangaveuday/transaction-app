3a785912bea1225c06898266219da5c769cae213
malangaveuday <malangaveuday23@gmail.com>
2025-03-02 00:32:05 +0530

    Update connectDB.ts

--- src/db/connectDB.ts
+++ src/db/connectDB.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    const DB_URL = process.env.DATABASE_URL || "mongodb+srv://malangaveuday23:EWbxj95AZl1w8lej@learningmongo.zslzz.mongodb.net/paytm_clone?retryWrites=true&w=majority";
+    const DB_URL = process.env.DATABASE_URL || "mongo_connection_url";
     try {
         if (DB_URL) {
             const DB = await mongoose.connect(DB_URL, {});
